dd2447c181cc18d55a0e7f72d0729d7e4e96060c
Emily McBride <emcbride@komodosystems.com>
2025-06-18 15:09:18 -0600

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/emily-soderquist-site/',
   plugins: [
     vue(),
     vueDevTools(),
